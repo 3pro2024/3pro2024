0877efc0c4e2daff05eddcbb801309e8d19c6fb2
nyk9 <eijikagimoto@gmail.com>
2025-01-26 19:29:52 +0900

    refactor(layout): Remove unused ModeToggle import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/layouts/header/theme-provider";
-import { ModeToggle } from "@/components/layouts/header/mode-toggle";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
