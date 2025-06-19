3cd9ee6fe25c5af588db08dcbd41de86a1e3aa1c
ItogaTaiyou <[22r3020185Z@stu.teikyo-u.ac.jp]>
2025-06-19 14:45:23 +0900

    fix:  buttonコンポーネント変更
    buttonコンポーネント変更しました。
    関数名はcreateButtonHTMLです。

--- src/components/button/button.ts
+++ src/components/button/button.ts
@@ -1,12 +1,3 @@
-/**
- * ボタンを生成して返す関数
- * @param label ボタンに表示するテキスト
- * @param onClick クリック時に実行する関数
- * @returns HTMLButtonElement
- */
-export function createButton(label: string, onClick: () => void): HTMLButtonElement {
-  const button = document.createElement("button");
-  button.textContent = label;
-  button.addEventListener("click", onClick);
-  return button;
+export function createButtonHTML(label: string, onClicks: string): string{
+    return `<button onclock="${onClicks}">${label}</button>`;
 }
