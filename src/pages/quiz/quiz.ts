248cde3b96c6c9095034d0a620c5186ed3827fba
ItogaTaiyou <[22r3020185Z@stu.teikyo-u.ac.jp]>
2025-07-31 14:03:02 +0900

    [WIP]: クイズ画面のボタン作成
    クイズ画面に終了ボタンを追加
    アラートのokボタンを押すとタイトルに戻ります
    また、modeselectで設定したmode・difficultyを取得できるようにしました

---
+++ src/pages/quiz/quiz.ts
@@ -0,0 +1,19 @@
+function retire(){// 終了ボタンを押したときの処理
+    const result = confirm("クイズを終了しますか？");
+    if (result) {
+        location.href = "../title/index.html";
+    }
+}
+// グローバルスコープに関数を公開
+(window as any).retire = retire;
+
+const urlParams = new URLSearchParams(location.search);
+// URLからクエリ文字列（URLパラメータ）を取得・扱いやすい形に変換
+const mode = urlParams.get('mode');
+// クエリパラメータからmodeを取得
+const difficulty = urlParams.get('difficulty');
+// クエリパラメータからdifficultyを取得
+
+console.log('mode:', mode);
+console.log('difficulty:', difficulty);
+
